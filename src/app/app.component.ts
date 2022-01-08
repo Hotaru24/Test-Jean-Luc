b4b2a4794ec25c0e9c15898fc92a983a87b654b1
Florent <florent.pasquet@beecoming.fr>
2022-01-08 16:39:12 +0100

    numbers convert to Jean-Luc

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Test-Jean-Luc';
+  numberList: number[] = [];
+  listToDIsplay: string[] = [];
+
+  constructor() { }
+  
+  ngOnInit() {  
+    for (let i = 0; i < 10; i++) {
+      this.numberList.push(Math.round(-1000 + (Math.random() * 2000)));
+    }    
+    this.numberList.forEach(number => this.numberToJeanLuc(number));
+  }
+
+  numberToJeanLuc(number: number) {
+    let result: string;
+    if (number%3 == 0 && number%5 !== 0) {
+      result = "Jean";
+    } else if (number%3 !== 0 && number%5 == 0) {
+      result = "Luc";
+    } else if (number%3 == 0 && number%5 == 0) {
+      result = "Jean-Luc";
+    } else {
+      result = "n";
+    }
+    this.listToDIsplay.push(result);
+    return result;
+  }
+  
 }
